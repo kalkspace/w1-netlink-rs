9241d672c16a8ba44ea845b4100282daa71313e2
Marcus Weiner <marcus.weiner@gmail.com>
2022-01-23 20:53:50 +0000

    Finish cmd impl

--- src/command.rs
+++ src/command.rs
@@ -1,7 +1,13 @@
+use std::mem;
+
 use crate::{message::W1MessageHeader, Deserializable, InvalidValue, Serializable};
+
+use self::raw::W1NetlinkCmd;
 
 mod raw {
     //! Taken from https://www.kernel.org/doc/Documentation/w1/w1.netlink
+
+    use safe_transmute::TriviallyTransmutable;
 
     pub mod constants {
         pub const W1_CMD_READ: u8 = 0;
@@ -18,14 +24,18 @@ mod raw {
     }
 
     /// Command for given master or slave device
+    #[repr(C)]
+    #[derive(Clone, Copy)]
     pub struct W1NetlinkCmd {
         /// Command opcode. See also [constants].
-        cmd: u8,
+        pub cmd: u8,
         /// reserved
-        _res: u8,
+        pub _res: u8,
         /// length of data for this command
-        len: u16,
+        pub len: u16,
     }
+
+    unsafe impl TriviallyTransmutable for W1NetlinkCmd {}
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -92,6 +102,8 @@ pub enum W1NetlinkCommand {
 }
 
 impl W1NetlinkCommand {
+    pub const HEADER_LEN: usize = mem::size_of::<W1NetlinkCmd>();
+
     fn cmd_type(&self) -> W1CommandType {
         match self {
             W1NetlinkCommand::Write(_) => W1CommandType::Write,
@@ -106,23 +118,83 @@ impl W1NetlinkCommand {
 }
 
 #[derive(Debug, thiserror::Error)]
-pub enum DeserializeError {}
+pub enum DeserializeError {
+    #[error("Invalid command type")]
+    InvalidType(#[from] InvalidValue),
+
+    #[error("Unable to read header: {0}")]
+    InvalidHeader(safe_transmute::Error<'static, u8, W1NetlinkCmd>),
+}
 
 impl Deserializable for W1NetlinkCommand {
     type Header = W1MessageHeader;
     type Error = DeserializeError;
 
-    fn deserialize(header: &Self::Header, payload: &[u8]) -> Result<(Self, usize), Self::Error> {
-        todo!()
+    fn deserialize(_header: &Self::Header, payload: &[u8]) -> Result<(Self, usize), Self::Error> {
+        let (header, payload) = payload.split_at(mem::size_of::<W1NetlinkCmd>());
+        let W1NetlinkCmd { cmd, len, .. } = safe_transmute::transmute_one_pedantic(header)
+            .map_err(|e| Self::Error::InvalidHeader(e.without_src()))?;
+
+        let cmd = match W1CommandType::try_from(cmd)? {
+            W1CommandType::Read => {
+                let payload = Some(len).filter(|l| *l > 0).map(|_| payload.to_vec());
+                Self::Read(payload)
+            }
+            W1CommandType::Write => {
+                let payload = payload.to_vec();
+                Self::Write(payload)
+            }
+            W1CommandType::Search => Self::Search,
+            W1CommandType::AlarmSearch => Self::AlarmSearch,
+            W1CommandType::Touch => Self::Touch,
+            W1CommandType::Reset => Self::Reset,
+            W1CommandType::SlaveAdd => unimplemented!(),
+            W1CommandType::SlaveRemove => unimplemented!(),
+            W1CommandType::ListSlaves => Self::ListSlaves,
+        };
+        Ok((cmd, len as usize))
     }
 }
 
 impl Serializable for W1NetlinkCommand {
     fn buffer_len(&self) -> usize {
-        todo!()
+        let inner = match self {
+            W1NetlinkCommand::Write(pl) => pl.len(),
+            W1NetlinkCommand::Read(pl) => pl.as_ref().map(Vec::len).unwrap_or_default(),
+            W1NetlinkCommand::Search => 0,
+            W1NetlinkCommand::AlarmSearch => 0,
+            W1NetlinkCommand::Touch => 0,
+            W1NetlinkCommand::Reset => 0,
+            W1NetlinkCommand::ListSlaves => todo!(),
+        };
+        inner + Self::HEADER_LEN
     }
 
     fn serialize(&self, buffer: &mut [u8]) {
-        todo!()
+        let cmd_type = self.cmd_type();
+        let len = (self.buffer_len() - Self::HEADER_LEN) as u16;
+        let raw = W1NetlinkCmd {
+            cmd: cmd_type.into(),
+            _res: Default::default(),
+            len,
+        };
+
+        let msg = safe_transmute::transmute_one_to_bytes(&raw);
+        debug_assert_eq!(mem::size_of::<W1NetlinkCmd>(), Self::HEADER_LEN);
+        buffer[0..Self::HEADER_LEN].copy_from_slice(msg);
+
+        match self {
+            W1NetlinkCommand::Write(pl) => buffer[Self::HEADER_LEN..].copy_from_slice(pl),
+            W1NetlinkCommand::Read(pl) => {
+                if let Some(pl) = pl {
+                    buffer[Self::HEADER_LEN..].copy_from_slice(pl);
+                }
+            }
+            W1NetlinkCommand::Search => {}
+            W1NetlinkCommand::AlarmSearch => {}
+            W1NetlinkCommand::Touch => {}
+            W1NetlinkCommand::Reset => {}
+            W1NetlinkCommand::ListSlaves => todo!(),
+        }
     }
 }
